35383a80e62ed85031551db3aea7f16b071c712b
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-07-12 07:11:00 +0100

    responsive done

--- src/websiteLandingPage/home/CallToAction.jsx
+++ src/websiteLandingPage/home/CallToAction.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const CallToAction = () => {
   return (
     <div className='pt-5'>
-        <div className='bg-orange-400 md:mt-0-sm px-4  mx-auto max-w-screen-xl lg:px-6'>
+        <div className='bg-orange-400 md:mt-0-sm px-4 sm:w-min-screen mx-auto max-w-screen-xl lg:px-6'>
           <div className='flex flex-row justify-between'>           
             <div className='pl-16 text-2xl lg:text-6xl text-center md:text-left pt-16 font-serif justify-start flex flex-col gap-8'>
               Experience the joy of <br /> 
@@ -13,7 +13,14 @@ const CallToAction = () => {
               Sign up now to unlock <br /> 
               a world of culinary <br /> 
               delights at your fingertips
-             <Link to="/signup"> <button type="button" className="flex justify-center ml-20 text-orange-400 bg-white rounded-lg w-80 h-14 text-center font-bold text-3xl items-center">JOIN US TODAY </button></Link>
+                <Link to="/signup">
+                    <button type="button" className="flex justify-center ml-20 text-orange-400 bg-white rounded-lg w-52 lg:w-80 lg:h-14 text-center font-bold text-3xl items-center">
+                        <span className="block lg:hidden">Sign Up</span>
+                        <span className="hidden lg:block">JOIN US TODAY</span>
+                    </button>
+                </Link>
+
+                {/*<Link to="/signup"> <button type="button" className="flex justify-center ml-20 text-orange-400 bg-white rounded-lg w-52 lg:w-80 lg:h-14 text-center font-bold text-3xl items-center">JOIN US TODAY </button></Link>*/}
             </div>
             <div className='flex pt-96 '>
               <img className='lg:w-[300px] w-[200px]' src={call} alt="" />
